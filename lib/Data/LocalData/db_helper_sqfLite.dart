e06d0ae57033f2e47e1f84e9630a4d2f8cdd88c8
raeeskhan371 <raeeskhann371@gmail.com>
2026-01-11 16:19:09 +0500

    Database Added

--- lib/Data/LocalData/db_helper_sqfLite.dart
+++ lib/Data/LocalData/db_helper_sqfLite.dart
@@ -1,46 +1,104 @@
-void main(){
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 
-  // int result =add(20, 40);
-  // String Chekced=Even(6);
+class  DbHelper{
 
-  List numbers=[10,20,30,40,];
-  List names=["raees","fahad","faisal"];
+  DbHelper._();
 
-  numbers.addAll(names);
-  names.addAll(numbers);
-  print(numbers);
-  print(names);
+  static final DbHelper getinstance=DbHelper._();
 
-
-
-
-
-
-}
-
+  static final String Table_Note="Note";
+  static final String Table_Colum_SEno="SEno";
+  static final String Table_Colum_Title="Title";
+  static final String Table_Colum_Description="Description";
+  static final String Table_Colum_Date="DueDate";
+  static final String Table_Colum_Checked="Checked";
 
 
 
 
-int add(int a,int b) {
-  int sum = a + b;
-  print(sum);
-  return sum;
-}
+  /// Global Variables
+  Database? myDb;
+
+  /// Table Note
 
 
-String Even(int number) {
-  if (number % 2 == 0) {
-    print(number);
 
-    return "Even number";
-  } else{
-    return "Odd number";}
-}
+  /// Now Create OpenDb function
+
+   Future<Database> getDB ()async{
+
+     if(myDb!=null){
+       return myDb!;
+     }else {
+        myDb=await openDB();
+        return myDb!;
+
+     }
 
 
-void namePrint(String name){
-  print(name);
+   }
+
+
+   Future<Database> openDB()async{
+
+    Directory appDirectory = await getApplicationDocumentsDirectory();
+    String dbpath=join(appDirectory.path,"TodoApp");
+    return await openDatabase(dbpath,onCreate:(db,version){
+      db.execute("create table $Table_Note($Table_Colum_SEno INTEGER PRIMARY KEY AUTOINCREMENT,$Table_Colum_Title TEXT,$Table_Colum_Description TEXT,$Table_Colum_Date TEXT,$Table_Colum_Checked INTEGER) "
+      );
+
+
+    } ,version: 1);
+
+   }
+
+   /// All queries
+
+  /// Create Query
+  Future<bool>addnote({required String title,required String description,required String date,required int Checked})async{
+
+    var db= await getDB();
+       int roweffected= await db.insert(Table_Note, {
+
+         Table_Colum_Title:title,
+         Table_Colum_Description:description,
+         Table_Colum_Date: date,
+         Table_Colum_Checked:Checked,
+       });
+       return roweffected>0;
+  }
+  /// Read Query
+  Future<List<Map<String,dynamic>>> readnote()async{
+
+     var db= await getDB();
+     List<Map<String,dynamic>> note=await db.query(Table_Note);
+     return note;
+  }
+
+  /// Update Query
+  Future<bool>updateNote({required int seNo,required String title,required String description,required String date,required int Checked})async{
+     var db=await getDB();
+     int rowseffected =await db.update(Table_Note, {
+       Table_Colum_Title:title,
+       Table_Colum_Description:description,
+       Table_Colum_Date:date,
+       Table_Colum_Checked:Checked,
+     },
+     where: "$Table_Colum_SEno=?",whereArgs: [seNo]
+     );
+     return rowseffected>0;
+
+  }
+  /// Delete Query
+  Future<bool> deletenote({required int seNo})async{
+    
+     var db=await getDB();
+     int rowseffected=await db.delete(Table_Note,where: "$Table_Colum_SEno=?",whereArgs: [seNo]);
+     return rowseffected>0;
+  }
 
 }
--- lib/Screen/Home_Screen.dart
+++ lib/Screen/Home_Screen.dart
@@ -183,8 +183,132 @@ class _HomeScreenState extends State<HomeScreen> {
                                   ),
                                 ),
                                 SizedBox(width: 32),
-                                Icon(FontAwesomeIcons.penToSquare,
-                                    color: Colors.indigo, size: 18),
+                                GestureDetector(
+                                  onTap: (){
+
+                                  },
+
+                                  child: GestureDetector( onTap: (){
+                                     showModalBottomSheet(context: context, builder:(context) {
+                                       return Container(
+                                         width: double.infinity,
+                                         child: Padding(
+                                           padding: const EdgeInsets.only(left: 20,right: 20,),
+                                           child: SingleChildScrollView(
+                                             child: Container(
+                                               child: Column(
+                                                 mainAxisAlignment: MainAxisAlignment.center,
+                                                 children: [
+                                                   SizedBox(height: 20,),
+                                                   /// Update Task Text
+                                                   Text("Update Task",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                                                   SizedBox(height: 10,),
+                                                   /// TextField Title
+                                                   TextFormField(
+                                                       decoration: InputDecoration(
+                                                         prefixIcon: Icon(Icons.title,color: Colors.grey,),
+                                                         labelText: "Title",
+                                                         hintText: "Enter Title",
+                                                         enabledBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(10),
+                                                             borderSide: BorderSide(color:Colors.grey.shade500,width: 1)
+                                                         ),
+                                                         focusedBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(15),
+                                                             borderSide: BorderSide(color:Colors.blue,width: 1)
+                                                         ),
+                                                       )
+                                                   ),
+                                                   /// TextField Description
+                                                   const SizedBox(height:20,),
+                                                   TextFormField(
+                                                       maxLines: 2,
+                                                       textAlignVertical: TextAlignVertical.center,
+                                                       decoration: InputDecoration(
+                                                         contentPadding: EdgeInsets.only(top: 30),
+                                                         prefixIcon: Icon(Icons.description_outlined,color: Colors.grey,),
+                                                         labelText: "Description",
+                                                         hintText: "Enter Task Details...",
+                                                         enabledBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(10),
+                                                             borderSide: BorderSide(color:Colors.grey.shade500,width: 1)
+                                                         ),
+                                                         focusedBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(15),
+                                                             borderSide: BorderSide(color:Colors.blue,width: 1)
+                                                         ),
+                                                       )
+                                                   ),
+                                                   const SizedBox(height:20,),
+                                                   /// DueDate Field
+                                                   TextFormField(
+                                                     keyboardType: TextInputType.number,
+                                                       readOnly: false,
+                                                       textAlignVertical: TextAlignVertical.center,
+                                                       decoration: InputDecoration(
+                                                         prefixIcon: Icon(Icons.calendar_month,color: Colors.grey,),
+                                                         hintText: "Due Date",
+                                                         enabledBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(5),
+                                                             borderSide: BorderSide(color:Colors.grey.shade500,width: 1)
+                                                         ),
+                                                         focusedBorder: OutlineInputBorder(
+                                                             borderRadius: BorderRadius.circular(5),
+                                                             borderSide: BorderSide(color:Colors.blue,width: 1)
+                                                         ),
+                                                       )
+                                                   ),
+                                                   SizedBox(height: 20,),
+                                                   /// Add Task Button
+                                                   Row(
+                                                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                     children: [
+                                                       ElevatedButton(
+                                                         style: ElevatedButton.styleFrom(
+                                                           fixedSize: Size(150, 10),
+                                                           backgroundColor: Colors.indigoAccent,
+                                                           foregroundColor: Colors.white,
+                                                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                                         ),
+                                                         onPressed: (){
+
+                                                           /// add function soon
+
+                                                         },
+                                                         child: Text("Update Task",style: TextStyle(fontSize: 18,),
+                                                         ),
+                                                       ),
+                                                       /// Cancel Button
+                                                       ElevatedButton(
+                                                         style: ElevatedButton.styleFrom(
+                                                           fixedSize: Size(150, 10),
+                                                           backgroundColor: Colors.red,
+                                                           foregroundColor: Colors.white,
+                                                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                                         ),
+                                                         onPressed: (){
+                                                           Navigator.of(context,rootNavigator: true).pop();
+                                                         },
+                                                         child: Text("Cancel",style: TextStyle(fontSize: 18,),
+                                                         ),
+                                                       ),
+                                                     ],
+                                                   )
+                                               
+                                                 ],
+                                               ),
+                                             ),
+                                           ),
+                                         ),
+
+                                       );
+
+                                     });
+                                  },
+                                    child: Icon(FontAwesomeIcons.penToSquare,
+                                        color: Colors.indigo, size: 18),
+                                  ),
+                                ),
                                 SizedBox(width: 20),
                                 Icon(FontAwesomeIcons.trash,
                                     color: Colors.red, size: 18),
